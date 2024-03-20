3897c02fc7548b48bdcb6d230a417a4790f60ddc
Saravana Kumar <128911354+Santa-28@users.noreply.github.com>
2024-03-20 21:16:59 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Find Last Digit Of a^b for Large Numbers/find-last-digit-of-ab-for-large-numbers.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+
+class Solution:
+    def getLastDigit(self, a, b):
+          # Convert 'a' to an integer and take the modulus by 10 to obtain the last digit.
+        a = int(a) % 10  
+        b = int(b)
+        
+        # Use exponentiation to calculate (a^b) mod 10
+        result = pow(a, b, 10)  
+        return result
+        
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        a,b=map(str,input().split())
+        
+        ob = Solution()
+        print(ob.getLastDigit(a,b))
+# } Driver Code Ends
