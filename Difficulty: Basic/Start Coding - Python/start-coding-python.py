9ebbf462b9b505ca575281028910e107f40a2ec6
Saravana Kumar <128911354+Santa-28@users.noreply.github.com>
2025-02-28 20:07:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Start Coding - Python/start-coding-python.py
@@ -0,0 +1,13 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+# You need to just print Hello World
+
+print("Hello World")
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
